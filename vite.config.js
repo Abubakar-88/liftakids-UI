4eca1a7f8fbb41c99cd753fb4f9ade994c7ffdd0
Abu Bakar <siddique0153@gmail.com>
2025-09-30 12:32:39 +0600

    logo add

--- vite.config.js
+++ vite.config.js
@@ -15,12 +15,12 @@ export default defineConfig({
         theme_color: '#2563eb',
         icons: [
           {
-            src: '/vite.svg',
+            src: '/liftakdis.svg',
             sizes: '192x192',
             type: 'image/svg+xml'
           },
           {
-            src: '/vite.svg',
+            src: '/liftakdis.svg',
             sizes: '512x512', 
             type: 'image/svg+xml'
           }
