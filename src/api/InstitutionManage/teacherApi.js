3b5fa04ac19e4123c38b54ef3c7167ec42704ddf
Abu Bakar <siddique0153@gmail.com>
2026-09-14 09:06:29 +0600

    API Implimenttion url issue fix

--- src/api/InstitutionManage/teacherApi.js
+++ src/api/InstitutionManage/teacherApi.js
@@ -1,7 +1,7 @@
 // api/teacherApi.js
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8082/LiftAKids/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
